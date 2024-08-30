70c378003ef7355738964f46dd3659fa9a26c2eb
ElodieMillan <elodyy.millan@gmail.com>
2024-08-30 15:01:12 +0200

    correct example.py

--- micro/build/CMakeFiles/microdrive.dir/DependInfo.cmake
+++ micro/build/CMakeFiles/microdrive.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/m.lavaud/Documents/joystick_MCL/micro/microdrive_pybind.cpp" "CMakeFiles/microdrive.dir/microdrive_pybind.cpp.o" "gcc" "CMakeFiles/microdrive.dir/microdrive_pybind.cpp.o.d"
+  "/home/brownianxgames/joystick/joystick_MCL/micro/microdrive_pybind.cpp" "CMakeFiles/microdrive.dir/microdrive_pybind.cpp.o" "gcc" "CMakeFiles/microdrive.dir/microdrive_pybind.cpp.o.d"
   )
 
 # Targets to which this target links.
--- micro/build/cmake_install.cmake
+++ micro/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/m.lavaud/Documents/joystick_MCL/micro
+# Install script for directory: /home/brownianxgames/joystick/joystick_MCL/micro
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/m.lavaud/Documents/joystick_MCL/micro/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/brownianxgames/joystick/joystick_MCL/micro/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
